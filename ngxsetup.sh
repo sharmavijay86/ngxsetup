7feb2f9a1daefdec874d4d1b6d5efa0b6e3b3445
Vijay K <sharmavijay86@gmail.com>
2024-03-20 16:41:01 +0530

    Update ngxsetup.sh

--- ngxsetup.sh
+++ ngxsetup.sh
@@ -112,5 +112,6 @@ history -c
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+sudo apt auto-remove -y
 # We're done !
 echo "Installation done."
